737fe5b69fb79483fa999ad112dfc1a61bc11cb4
刘腾辉 <354151733@qq.com>
2017-07-24 19:24:31 +0800

    optimize udpServer

--- udpServer.py
+++ udpServer.py
@@ -13,6 +13,33 @@ import twisted
 from twisted.internet.protocol import DatagramProtocol
 from utils.printl import printl
 from config.base import logfilePath
+from Queue import Queue
+from twisted.internet import reactor
+
+caches={}
+
+
+def savedata(client):
+    cache=caches.get(client,None)
+    queue=cache["queue"]
+    datas=ByteIO()
+    datalen=queue.qsize()
+    while True:
+        cur_time=time.time
+        if cur_time-time>10:
+            break;
+
+
+    data=queue.get(data)
+    datas.write(data)
+
+    with open("ffff.dat","wb") as f
+        f.write
+
+    del cache
+
+
+
 
 class udpServer(DatagramProtocol):
 
@@ -27,13 +54,34 @@ class udpServer(DatagramProtocol):
     def stopProtocol(self):  # stop udpServer
         printl(self.log, logfilePath, toConsole=False)  # 记录而不打印控制台
 
-    def datagramReceived(self, data, (host, port)):
+    def _get_data_index(self,data)
+        pass
+        return data
+
+
+
+    def datagramReceived(self, data, client):
         if port != self.lastPort:  # 如果端口和上一次不一样
             if self.log:
                 printl(self.log, logfilePath, toConsole=False)  # 记录而不打印控制台
             self.lastPort = port  # 重置上次端口
             self.datas = []  # 清空数据
             sys.stdout.write('\n')  # 控制台换行重新输出
+
+        cache=caches.get(client,None)
+        if cache is None:
+            cache={
+                "queue":Queue(),
+                "join_time":time.time()
+                }
+            caches[client]=cache
+            reactor.callInThread(savedata,client)
+
+        queue=cache["queue"]
+        queue.put(data)
+
+
+        
 
         time.sleep(1)  # 模拟数据处理所需时间
 
